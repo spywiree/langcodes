07944b494539236ad5a78ffbb417f053d161a137
Spywire <79043321+spywiree-priv@users.noreply.github.com>
2024-05-20 19:34:42 +0200

    Add LanguagesToCodes()

--- generate/generate.py
+++ generate/generate.py
@@ -1,39 +1,74 @@
-import requests, json, os
+import requests, json, os, io
 
 URL = "https://raw.githubusercontent.com/nidhaloff/deep-translator/master/deep_translator/constants.py"
+
+
+def tab(n: int = 1) -> str:
+    return n * "\t"
+
+
+def nl(n: int = 1) -> str:
+    return n * "\n"
+
+
+def add_consts(
+    f: io.TextIOWrapper,
+    langs: dict[str, str],
+    type_name: str,
+):
+    f.write(f"type {type_name} string" + nl(2))
+    f.write("const (" + nl())
+    f.write(tab() + f'DETECT_LANGUAGE {type_name} = "auto"' + nl(2))
+    for k, value in langs.items():
+        key = k.upper().replace(" ", "_").replace("(", "").replace(")", "")
+        f.write(tab() + f'{key} {type_name} = "{value}"' + nl())
+
+    f.write(")" + nl(2))
+
+
+def add_supported_langs(f: io.TextIOWrapper, langs: dict[str, str]):
+    supported_langs = json.dumps(list(langs.keys()))[1:-1]
+    supported_langs_go = "[]string{" + supported_langs + "}"
+
+    f.write("func SupportedLanguages() []string {" + nl())
+    f.write(tab() + f"return {supported_langs_go}" + nl())
+    f.write("}" + nl(2))
+
+
+def add_languages_to_codes(
+    f: io.TextIOWrapper,
+    langs: dict[str, str],
+):
+    f.write("func LanguagesToCodes() map[string]string {" + nl())
+    f.write(tab() + "return map[string]string{" + nl())
+
+    for k, value in langs.items():
+        key = k.upper().replace(" ", "_").replace("(", "").replace(")", "")
+        f.write(tab(2) + f'"{key}" : "{value}",' + nl())
+    f.write(tab() + "}" + nl())
+
+    f.write("}")
 
 
 def main(path: str, pkg_name: str, type_name: str):
     constants = requests.get(URL).text
 
-    g = {}
-    exec(constants, g)
-    langs: dict[str, str] = g["GOOGLE_LANGUAGES_TO_CODES"]
+    variables = {}
+    exec(constants, variables)
+    langs: dict[str, str] = variables["GOOGLE_LANGUAGES_TO_CODES"]
 
     try:
         os.remove(path)
     except FileNotFoundError:
         pass
     with open(path, mode="a", encoding="utf-8") as f:
-        f.write(f"package {pkg_name}" + "\n\n")
-        f.write(f"type {type_name} string" + "\n\n")
-        f.write("const (" + "\n")
+        f.write(f"package {pkg_name}" + nl(2))
 
-        f.write("\t" + f'DETECT_LANGUAGE {type_name} = "auto"' + "\n\n")
-        for k, value in langs.items():
-            key = k.upper().replace(" ", "_").replace("(", "").replace(")", "")
-            f.write("\t" + f'{key} {type_name} = "{value}"' + "\n")
+        add_consts(f, langs, type_name)
+        add_supported_langs(f, langs)
+        add_languages_to_codes(f, langs)
 
-        f.write(")" + "\n\n")
-
-        supported_langs = json.dumps(list(langs.keys()))[1:-1]
-        supported_langs_goarray = "[]string{" + supported_langs + "}"
-
-        f.write("func SupportedLanguages() []string {" + "\n")
-        f.write("\t" + f"return {supported_langs_goarray}" + "\n")
-        f.write("}")
-
-        f.write("\n")
+        f.write(nl())
 
     os.system(f'go fmt "{path}"')
 
